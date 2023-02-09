6127c3a846d02fa3eb5a9431f30fe5906007ed2b
Dani <hostnameadmin@protonmail.com>
2023-02-09 21:10:10 +0100

    Redirect to login and registration added

--- app/src/main/java/hu/petrik/gorillago_android/MainActivity.java
+++ app/src/main/java/hu/petrik/gorillago_android/MainActivity.java
@@ -1,14 +1,48 @@
 package hu.petrik.gorillago_android;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MaterialButton buttonLogin, buttonRegistration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         setContentView(R.layout.activity_main);
+        init();
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Login.class));
+            }
+        });
+
+        buttonRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Registration.class));
+            }
+        });
+    }
+
+    private void init()
+    {
+        buttonLogin = findViewById(R.id.loginButton);
+        buttonRegistration= findViewById(R.id.registrationButton);
+
     }
 }
