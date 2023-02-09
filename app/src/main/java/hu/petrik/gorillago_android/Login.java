a940fd07404706b3d2e3e6620c0b600f4a000b39
Dani <hostnameadmin@protonmail.com>
2023-02-09 21:33:24 +0100

    Back button added to login

--- app/src/main/java/hu/petrik/gorillago_android/Login.java
+++ app/src/main/java/hu/petrik/gorillago_android/Login.java
@@ -3,11 +3,17 @@ package hu.petrik.gorillago_android;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.google.android.material.button.MaterialButton;
+
 public class Login extends AppCompatActivity {
+
+    private MaterialButton buttonBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,19 @@ public class Login extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
         setContentView(R.layout.activity_login);
+        init();
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, MainActivity.class));
+            }
+        });
+    }
+
+
+    private void init()
+    {
+        buttonBack = findViewById(R.id.buttonBack);
     }
 }
