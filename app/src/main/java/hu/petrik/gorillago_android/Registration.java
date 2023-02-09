ded4546cc394de40bf16459103b7f90d1e6926c4
Dani <hostnameadmin@protonmail.com>
2023-02-09 15:58:35 +0100

    registration layout wip

---
+++ app/src/main/java/hu/petrik/gorillago_android/Registration.java
@@ -0,0 +1,14 @@
+package hu.petrik.gorillago_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
