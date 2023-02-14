3b281be6764942c6c99580e6b3e6537b0e869a4d
dani <f.dani2002@gmail.com>
2023-02-14 12:09:55 +0100

    Added logo, icon

--- app/src/main/java/hu/petrik/gorillago_android/Registration.java
+++ app/src/main/java/hu/petrik/gorillago_android/Registration.java
@@ -3,11 +3,20 @@ package hu.petrik.gorillago_android;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.datepicker.MaterialTextInputPicker;
 
 public class Registration extends AppCompatActivity {
+
+
+    private MaterialButton buttonBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +26,18 @@ public class Registration extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
         setContentView(R.layout.activity_registration);
+        init();
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Registration.this, MainActivity.class));
+            }
+        });
+    }
+
+    private void init()
+    {
+        buttonBack = findViewById(R.id.buttonBack);
     }
 }
