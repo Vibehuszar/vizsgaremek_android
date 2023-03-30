51d0c2a921f37ddaab233d5a6af3e846d6df1741
dani <f.dani2002@gmail.com>
2023-03-30 13:58:38 +0200

    Homepage rework + added themes

--- app/src/main/java/hu/petrik/gorillago_android/GorillaGoActivity.java
+++ app/src/main/java/hu/petrik/gorillago_android/GorillaGoActivity.java
@@ -3,24 +3,38 @@ package hu.petrik.gorillago_android;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.google.gson.Gson;
 import com.squareup.picasso.Picasso;
 
@@ -32,30 +46,101 @@ import java.util.List;
 public class GorillaGoActivity extends AppCompatActivity {
 
     private ListView listViewRestaurants;
+    private TextView textViewRecommended1, textViewRecommended2, textViewRecommended3;
+
+    private ImageView imageViewRecommended1, imageViewRecommended2, imageViewRecommended3;
+
+    private HorizontalScrollView horizontalScrollView;
     private List<Restaurant> restaurants = new ArrayList<>();
+
+    private Restaurant[] restaurantArray = new Restaurant[5];
+    private AlertDialog.Builder alert;
     private String url = "http://10.0.2.2:3000/restaurants";
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+    private Toolbar toolbar;
+
+    private FrameLayout frameLayout;
+
+    private NavigationView navigationView;
+    private TextView textViewTop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
         setContentView(R.layout.activity_gorilla_go);
-        init();
         RequestTask task = new RequestTask(url, "GET");
         task.execute();
+        init();
+        getSupportActionBar().hide();
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        frameLayout.setVisibility(View.GONE);
+                        horizontalScrollView.setVisibility(View.VISIBLE);
+                        textViewTop.setVisibility(View.VISIBLE);
+                        break;
+                    case R.id.account:
+                        frameLayout.setVisibility(View.VISIBLE);
+                        horizontalScrollView.setVisibility(View.GONE);
+                        textViewTop.setVisibility(View.GONE);
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AccountFragment()).commit();
+                        break;
+                    case R.id.logout:
+                        alert.show();
+                        break;
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
     }
-
     private void init() {
-        listViewRestaurants = findViewById(R.id.listViewRestaurants);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        linearLayoutManager.setOrientation(RecyclerView.HORIZONTAL);
-        listViewRestaurants.setAdapter(new RestaurantAdapter());
+        //listViewRestaurants = findViewById(R.id.listViewRestaurants);
+       // LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        //linearLayoutManager.setOrientation(RecyclerView.HORIZONTAL);
+        //listViewRestaurants.setAdapter(new RestaurantAdapter());
+        navigationView = findViewById(R.id.navigationView);
+        toolbar = findViewById(R.id.toolBar);
+        frameLayout = findViewById(R.id.fragment_container);
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        textViewTop = findViewById(R.id.textViewTop);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.nav_open, R.string.nav_close);
+        actionBarDrawerToggle.getDrawerArrowDrawable().setColor(Color.WHITE);
+        textViewRecommended1 = findViewById(R.id.textViewRecommended1);
+        textViewRecommended2 = findViewById(R.id.textViewRecommended2);
+        textViewRecommended3 = findViewById(R.id.textViewRecommended3);
+        imageViewRecommended1 = findViewById(R.id.imageViewRecommended1);
+        imageViewRecommended2 = findViewById(R.id.imageViewRecommended2);
+        imageViewRecommended3 = findViewById(R.id.imageViewRecommended3);
+        horizontalScrollView = findViewById(R.id.horizontalScrollView);
+        alert = new AlertDialog.Builder(GorillaGoActivity.this);
+        alert.setMessage("Biztos ki akar jelentkezni?")
+                .setPositiveButton("Nem", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int which){
+                    }
+                })
+                .setNegativeButton("Igen", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("MyPref", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.remove("token");
+                        editor.commit();
+                        startActivity(new Intent(GorillaGoActivity.this, LoginActivity.class));
+                        finish();
+                    }
+                })
+                .setCancelable(false)
+                .create();
     }
-
-    private class RestaurantAdapter extends ArrayAdapter<Restaurant> {
+    /*private class RestaurantAdapter extends ArrayAdapter<Restaurant> {
         public RestaurantAdapter() {
             super(GorillaGoActivity.this, R.layout.restaurantadapter, restaurants);
         }
@@ -70,27 +155,24 @@ public class GorillaGoActivity extends AppCompatActivity {
             ImageView imageViewRecommended = view.findViewById(R.id.imageViewRecommended);
             textViewRecommended.setText(actualRestaurant.getName());
             Picasso.get().load(actualRestaurant.getUrl()).into(imageViewRecommended);
+            System.out.println(actualRestaurant.getName());
+            System.out.println(actualRestaurant);
             return view;
         }
-    }
-
+    }*/
     private class RequestTask extends AsyncTask<Void, Void, Response> {
-
         String requestUrl;
         String requestType;
         String requestParams;
-
         public RequestTask(String requestUrl, String requestType, String requestParams) {
             this.requestUrl = requestUrl;
             this.requestType = requestType;
             this.requestParams = requestParams;
         }
-
         public RequestTask(String requestUrl, String requestType) {
             this.requestUrl = requestUrl;
             this.requestType = requestType;
         }
-
         @Override
         protected Response doInBackground(Void... voids) {
             Response response = null;
@@ -109,19 +191,27 @@ public class GorillaGoActivity extends AppCompatActivity {
             }
             return response;
         }
-
         @Override
         protected void onPostExecute(Response response) {
             super.onPostExecute(response);
             Gson converter = new Gson();
             if (response == null || response.getResponseCode() >= 400) {
-                Toast.makeText(GorillaGoActivity.this, "Hiba a Bejelentkezés során", Toast.LENGTH_SHORT).show();
+                Toast.makeText(GorillaGoActivity.this, "Hiba a lekérdezés során", Toast.LENGTH_SHORT).show();
             } else {
                 switch (requestType) {
                     case "GET":
                         Restaurant[] restaurantsArray = converter.fromJson(response.getContent(), Restaurant[].class);
                         restaurants.clear();
                         restaurants.addAll(Arrays.asList(restaurantsArray));
+                        for (int i = 0; i < restaurantsArray.length; i++) {
+                            restaurantArray[i] = restaurantsArray[i];
+                        }
+                        textViewRecommended1.setText(restaurantArray[0].getName());
+                        Picasso.get().load(restaurantArray[0].getUrl()).into(imageViewRecommended1);
+                        textViewRecommended2.setText(restaurantArray[1].getName());
+                        Picasso.get().load(restaurantArray[1].getUrl()).into(imageViewRecommended2);
+                        textViewRecommended3.setText(restaurantArray[2].getName());
+                        Picasso.get().load(restaurantArray[2].getUrl()).into(imageViewRecommended3);
                         break;
                 }
             }
